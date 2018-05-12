f968af2b567ce2982fa115ce7e8e4da2048f7c88
hengshuai.hu <hengshuai.hu@renren-inc.com>
2018-05-12 11:38:09 +0800

    feat: 首屏下多屏切换逻辑完成

--- index.js
+++ index.js
@@ -8,56 +8,67 @@ let timeDelay = (time=500)=>new Promise((resolve)=>{
         resolve();
     }, time);
 })
-
-console.log('xxxxxxxx');
-(async function (){
-    console.log('begin first screen')
-    // 延时0ms html
-    // 时刻100ms
-    await timeDelay(100);
-    console.log('100ms')
-    $('.p-box').removeClass('no-opacity').addClass('slide-in-7');
-
-    // 时刻600ms
+async function loadFrame(frameClass ='animate-frame-one') {
+    $(`.${frameClass} .part-screen.figure-content`).removeClass('no-opacity').addClass('slide-in-5');
     await timeDelay(500);
     console.log('600ms')
-    $('.frame-one .part1').removeClass('no-opacity').addClass('slide-in-1');
-    $('.frame-one .part2').removeClass('no-opacity').addClass('slide-in-2');
-    $('.frame-one .part3').removeClass('no-opacity').addClass('slide-in-3');
+    $(`.${frameClass} .part1`).removeClass('no-opacity').addClass('slide-in-1');
+    $(`.${frameClass} .part2`).removeClass('no-opacity').addClass('slide-in-2');
+    $(`.${frameClass} .part3`).removeClass('no-opacity').addClass('slide-in-3');
 
     // 时刻1000ms,进入浮动阶段
     await timeDelay(400);
     console.log('1000ms')
-    $('.frame-one .part1').removeClass('slide-in-1').addClass('move-1');
-    
+    $(`.${frameClass} .part1`).removeClass('slide-in-1').addClass('move-1');
+
     // 时刻1100ms + 100延时
     await timeDelay(200);
     console.log('1100ms')
-    $('.frame-one .part2').removeClass('slide-in-2').addClass('move-2');
+    $(`.${frameClass} .part2`).removeClass('slide-in-2').addClass('move-2');
 
     // 延时1200ms + 200延时
     await timeDelay(300);
     console.log('1200ms')
-    $('.frame-one .part3').removeClass('slide-in-3').addClass('move-3');
-    
+    $(`.${frameClass} .part3`).removeClass('slide-in-3').addClass('move-3');
+
     // 目测无限滚动时间为2s
     await timeDelay(2000);
     console.log('3200ms')
-    $('.frame-one .part1').removeClass('move-1').addClass('switch-out-1');
-    $('.frame-one .part2').removeClass('move-2').addClass('switch-out-1');
-    $('.frame-one .part3').removeClass('move-3').addClass('switch-out-1');
+    $(`.${frameClass} .part1`).removeClass('move-1').addClass('switch-out-1');
+    $(`.${frameClass} .part2`).removeClass('move-2').addClass('switch-out-1');
+    $(`.${frameClass} .part3`).removeClass('move-3').addClass('switch-out-1');
 
     await timeDelay(300);
     console.log('3500ms')
-    $('.frame-one .part1,.frame-one .part2,.frame-one .part3').removeClass('switch-out-1').addClass('no-opacity');
+    $(`.${frameClass} .part1,.${frameClass} .part2,.${frameClass} .part3`).removeClass('switch-out-1').addClass('no-opacity');
 
     await timeDelay(200);
     console.log('3700ms')
-    $('.frame-one .part-screen.figure-content').removeClass('slide-in-5').addClass('no-opacity');
-    // 总时长3800ms
-    console.log('first screen end');
+    $(`.${frameClass} .part-screen.figure-content`).removeClass('slide-in-5').addClass('switch-out-1');
+    await timeDelay(100);
+    $(`.${frameClass} .part-screen.figure-content`).removeClass('switch-out-1').addClass('no-opacity');
 
+}
 
-    console.log('begin loop')
+console.log('xxxxxxxx');
+let frameClassList = ['animate-frame-one', 'animate-frame-two', 'animate-frame-three','animate-frame-four'],
+    len = frameClassList.length,
+    currenFrameIndex = 0;
+(async function (){
+    await timeDelay(100);
+    $('.p-box').removeClass('no-opacity').addClass('slide-in-7');
+
+    setInterval(() => {
+        console.log('currenFrameIndex', currenFrameIndex);
+        if (currenFrameIndex >= len){
+            currenFrameIndex  = 0;
+        }
+        loadFrame(frameClassList[currenFrameIndex]);
+        currenFrameIndex++;
+    }, 4000);
+
+    // 初始化
+    loadFrame(frameClassList[0]);
+    currenFrameIndex++;
     
 })();
